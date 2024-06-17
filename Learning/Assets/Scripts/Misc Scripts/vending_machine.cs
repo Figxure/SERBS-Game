2708c57b100820f155843abccd934926093648a3
Figxure <www.abdulla25@gmail.com>
2024-06-16 22:56:20 -0400

    Vending Machine for health works!

--- Learning/Assets/Scripts/Misc Scripts/vending_machine.cs
+++ Learning/Assets/Scripts/Misc Scripts/vending_machine.cs
@@ -8,20 +8,32 @@ public class vending_machine : MonoBehaviour
 
     public Player player;
 
+    public Transform spawnPoint;
+
     private void OnTriggerStay(Collider other)
     {
-        while(true)
-        {
-            if(Input.GetKey(KeyCode.F))
-            {
-                Instantiate(HealthDrink);
 
-                if(player.currentHealth < player.maxHealth)
-                {
-                    player.currentHealth += 10;
-                }
-                    
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(HealthDrink);
+
+            HealthDrink.transform.position = spawnPoint.position;
+
+
+            if (player.currentHealth < player.maxHealth)
+            {
+                player.currentHealth = player.currentHealth + 10;
+                player.healthBar.SetHealth(player.currentHealth);
             }
+
+
+            Invoke("DeleteCans", 5f);
+
         }
+    }
+
+    void DeleteCans()
+    {
+        Destroy(HealthDrink);
     }
 }
