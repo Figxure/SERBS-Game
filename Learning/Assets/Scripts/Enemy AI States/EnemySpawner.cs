ab7bb565a12d710c8ebfa6f3323aacf52cc680c0
Figxure <www.abdulla25@gmail.com>
2024-06-19 03:26:38 -0400

    Half working enemy spawner

---
+++ Learning/Assets/Scripts/Enemy AI States/EnemySpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject Enemy;
+
+    public GameObject EnemyTrigger;
+
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(Enemy);
+
+        Enemy.transform.position = spawnPoint.position;
+
+        Destroy(EnemyTrigger);
+    }
+}
